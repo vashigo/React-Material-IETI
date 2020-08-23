904cbd4ed141853e5f72fd6b9cbfe115be3cbaf9
vashigo <andresdavid.vasquez@gmail.com>
2020-08-23 18:11:16 -0500

    part 2 all

--- src/App.js
+++ src/App.js
@@ -5,29 +5,47 @@ import './App.css';
 import Login from '../src/components/Login';
 import TodoApp from '../src/components/TodoApp';
 
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+
 class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = { 
-
+            isLoggedIn : false,
         };
     }
 
 
     render() {
 
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">TODO React App</h1>
-                </header>
-                <Login/>
-                <br/>
-                <br/>
+        const LoginView = () => (
+            <Login/>
+        );
+      
+        const TodoAppView = () => (
                 <TodoApp/>
-            </div>
+        );
+
+        return (
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">TODO React App</h1>
+                    </header>
+                        <br/>
+                        <br/>
+                    <body>
+                        <div>
+                            {!this.state.isLoggedIn && <Route exact path="/" component={LoginView}/>}
+                            {this.state.isLoggedIn && <Route path="/todo" component={TodoAppView}/>}
+                        </div>
+                    </body>
+
+
+                </div>
+            </Router>
         );
     }
 
